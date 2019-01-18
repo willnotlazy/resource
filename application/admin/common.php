3d40354dd35f5a29da81f33d4a30c118fe95e6c9
luojiangyuan <luojiangyuan@kaiyuan.net>
2019-01-18 16:29:28 +0800

    创建admin

---
+++ application/admin/common.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 欢迎
+ * Date: 2019/1/18
+ * Time: 15:40
+ */
---
+++ application/admin/controller/Index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 欢迎
+ * Date: 2019/1/18
+ * Time: 15:57
+ */
+
+namespace app\admin\controller;
+
+class Index extends Base
+{
+    public function Index()
+    {
+        return 'hello world';
+    }
+}
+?>
